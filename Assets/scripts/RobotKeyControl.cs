2aebc45f6147853e493f96fb162ba22d3b6acede
Leigh Pauls <leigh.pauls@gmail.com>
2013-12-18 01:28:05 -0500

    Basic cart motion

---
+++ Assets/scripts/RobotKeyControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotKeyControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public float WheelPower { get; private set; }
+	
+	// Update is called once per frame
+	void Update () {
+		const float maxWheelPower = 10000f;
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			WheelPower = maxWheelPower;
+		} else if (Input.GetKey(KeyCode.DownArrow)) {
+			WheelPower = -maxWheelPower;
+		} else {
+			WheelPower = 0.0f;
+		}
+		
+	}
+}
---
+++ Assets/scripts/WheelScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float power = transform
+			.parent
+			.gameObject
+			.GetComponentInChildren<RobotKeyControl>()
+			.WheelPower;
+		Debug.Log(power);
+		this.rigidbody.AddRelativeTorque(0, 0, power);
+	}
+}
