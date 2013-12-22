453942dbe62a3fd7faa355c41dc4753673ec5bc3
Leigh Pauls <leigh.pauls@gmail.com>
2013-12-22 02:04:02 -0500

    Initial commit of the simulation interface

---
+++ simulation_interface/src/com/leighpauls/SimulationInterface.java
@@ -0,0 +1,10 @@
+package com.leighpauls;
+
+/**
+ * The main file that gets run during a simulation
+ */
+public class SimulationInterface {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
